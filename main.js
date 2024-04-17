eb16ff0b0bfc525892a5e70efa48f621f2a7fb5c
FilippoBonanno <fbonanno15@gmail.com>
2024-04-17 16:13:49 +0200

    ages km prize_km

---
+++ main.js
@@ -0,0 +1,9 @@
+const km = prompt("Quanti km vuoi percorrere?"); 
+console.log( `Km da percorrere: ${km}` );
+
+const ages = prompt("Quanti anni hai?"); 
+console.log( `anni: ${ages}` );
+
+let prize_km = 0.21€
+
+let ticketprize = prize_km * km 
