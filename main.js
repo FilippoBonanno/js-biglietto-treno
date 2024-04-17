052333f1c78df318b5f0b98af004c8b8b3aed700
FilippoBonanno <fbonanno15@gmail.com>
2024-04-17 17:12:01 +0200

    bonus

--- main.js
+++ main.js
@@ -14,20 +14,21 @@ let discount40 = 0.4
 if (ages < 18) {
 
     let ticketprize = ((prize_km * km) - (prize_km * km * discount20))
-    console.log( `Prezzo biglietto: ${ticketprize.toFixed(1)}€` );
+    console.log( `Prezzo biglietto: ${ticketprize.toFixed(1)}€`);
+    document.getElementById("final-prize").innerText = `${ticketprize.toFixed(1)}€ Già scontato del 20%`;
 
 
 } else if (ages > 65 ) {
 
     let ticketprize = ((prize_km * km) - (prize_km * km * discount40))
-    console.log( `Prezzo biglietto: ${ticketprize.toFixed(1)}€` );
-    
-
+    console.log( `Prezzo biglietto: ${ticketprize.toFixed(1)}€`);
+    document.getElementById("final-prize").innerText = `${ticketprize.toFixed(1)}€ Già scontato del 40%`;
     
 
 } else {
 
     let ticketprize = prize_km * km 
-    console.log( `Prezzo biglietto: ${ticketprize.toFixed(1)}€` );
+    console.log( `Prezzo biglietto: ${ticketprize.toFixed(1)}€`);
+    document.getElementById("final-prize").innerText = `${ticketprize.toFixed(1)}€`;
 
 }
